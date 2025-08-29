4750addc1fe38932bea94cd122d719679792d709
Salvatore0609 <salvatoredesole1998@gmail.com>
2025-08-29 16:55:42 +0200

    Refactor formatting in astro.config.ts for improved readability

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  site: process.env.CI ? 'https://astro-shadcn-ui-template.vercel.app' : 'http://localhost:4321',
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  })]
-});
+  site: process.env.CI
+    ? 'https://astro-shadcn-ui-template.vercel.app'
+    : 'http://localhost:4321',
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
+  image: {
+    domains: ['images.ctfassets.net'],
+  },
+})
